39894842947105f8b75e1c82fa6dd07eb5ef44d2
Rafael <119387208+roughy713@users.noreply.github.com>
2023-09-21 22:33:21 +0800

    MainActivity.kt

--- src/main/java/ph/edu/uic/prelimexamdelloson/MainActivity.kt
+++ src/main/java/ph/edu/uic/prelimexamdelloson/MainActivity.kt
@@ -1,11 +1,39 @@
 package ph.edu.uic.prelimexamdelloson
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnLogout: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogout = findViewById(R.id.Button)
+        mAuth = FirebaseAuth.getInstance()
+
+        btnLogout.setOnClickListener {
+            logout()
+        }
+    }
+
+    private fun logout() {
+        mAuth.signOut()
+        Toast.makeText(this, "Logged out successfully", Toast.LENGTH_SHORT).show()
+
+        // Redirect the user to the login screen or any other desired screen
+        val intent = Intent(this, Login::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        finish()
     }
 }
